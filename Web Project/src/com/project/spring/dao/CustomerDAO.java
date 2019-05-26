18147a29c72a935cb89a4aed072cb560e597b051
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 15:33:12 +0530

    Import removed DAO

--- Web Project/src/com/project/spring/dao/CustomerDAO.java
+++ Web Project/src/com/project/spring/dao/CustomerDAO.java
@@ -2,8 +2,6 @@ package com.project.spring.dao;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.project.spring.entity.Customer;
 
 public interface CustomerDAO {
