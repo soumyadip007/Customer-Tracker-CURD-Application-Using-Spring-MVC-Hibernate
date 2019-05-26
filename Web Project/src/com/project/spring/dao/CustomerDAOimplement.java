d00249bc00e4c19c4da1f6e4a20f562d2e4689bb
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 14:10:44 +0530

    @Transactional & @Repository added with method()

--- Web Project/src/com/project/spring/dao/CustomerDAOimplement.java
+++ Web Project/src/com/project/spring/dao/CustomerDAOimplement.java
@@ -2,14 +2,45 @@ package com.project.spring.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.project.spring.entity.Customer;
 
+@Repository
 public class CustomerDAOimplement implements CustomerDAO {
 
+	@Autowired
+	private SessionFactory sessionfactory;
+	
+	
 	@Override
+	@Transactional
 	public List<Customer> getCustomer() {
 		
-		return null;
+		
+		//get the current hibernate session
+		Session session=(Session) sessionfactory.getSessionFactory();
+		
+		
+		//Create query
+		Query<Customer> query=session.createQuery("form customer",Customer.class);
+		
+	
+		//execute
+		List<Customer> customer=query.getResultList();
+		
+		
+		
+		//return the result
+		return customer;
+		
+	
 	}
 
 }
