f2ba4f6234d5a4e3bf6f1a83e96534974db111cb
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 13:57:23 +0530

    CustomerDAOimplement Class added

---
+++ Web Project/src/com/project/spring/dao/CustomerDAOimplement.java
@@ -0,0 +1,15 @@
+package com.project.spring.dao;
+
+import java.util.List;
+
+import com.project.spring.entity.Customer;
+
+public class CustomerDAOimplement implements CustomerDAO {
+
+	@Override
+	public List<Customer> getCustomer() {
+		
+		return null;
+	}
+
+}
