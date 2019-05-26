dde2975755ddc60d6a7618283ba2b6ff0779dccf
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 15:43:10 +0530

    Newly Added

--- Web Project/src/com/project/spring/entity/Customer.java
+++ Web Project/src/com/project/spring/entity/Customer.java
@@ -11,75 +11,59 @@ import javax.persistence.Table;
 @Table(name="customer")
 public class Customer {
 
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
-	
 	@Column(name="first_name")
-	private String firstname;
-	
+	private String firstName;
 	
 	@Column(name="last_name")
-	private String lastname;
-	
+	private String lastName;
 	
 	@Column(name="email")
 	private String email;
 	
-	
-	public Customer()
-	{
+	public Customer() {
 		
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-	public String getFirstname() {
-		return firstname;
+	public String getFirstName() {
+		return firstName;
 	}
 
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-
-	public String getLastname() {
-		return lastname;
+	public String getLastName() {
+		return lastName;
 	}
 
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email + "]";
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
-	
+		
 }
