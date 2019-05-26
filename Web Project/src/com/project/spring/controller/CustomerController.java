b60be724dc597b583812152ec8666fc2cf506223
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 13:41:42 +0530

    Class name CustomerController

--- Web Project/src/com/project/spring/controller/CustomerController.java
+++ Web Project/src/com/project/spring/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
-public class CustomerList {
+public class CustomerController {
 	
 	@RequestMapping("/list")
 	public String listCustomer(Model theModel)
