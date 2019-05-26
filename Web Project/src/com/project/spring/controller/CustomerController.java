38d4a8e641b9deb2a60f107d4363ce0a5b96819f
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 15:11:16 +0530

    Controller

--- Web Project/src/com/project/spring/controller/CustomerController.java
+++ Web Project/src/com/project/spring/controller/CustomerController.java
@@ -1,17 +1,39 @@
 package com.project.spring.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.spring.dao.CustomerDAO;
+import com.project.spring.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	//Inject the DAO into the controller
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+		
+	
 	@RequestMapping("/list")
 	public String listCustomer(Model theModel)
 	{
+		
+		//get the customer from the DAO
+		List<Customer> theCustomer=customerDAO.getCustomer();
+		
+		
+		//Add customer to model
+		theModel.addAttribute("customer",theCustomer);
+		
+		
+		
 		return "listcustomer";
 	}
 }
