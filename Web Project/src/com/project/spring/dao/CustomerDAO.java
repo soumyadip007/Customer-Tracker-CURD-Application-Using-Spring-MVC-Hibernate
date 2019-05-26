a9d3da84c01b3a582612a581e9704b60632f8d55
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 13:56:21 +0530

    Interface CustomerDAO Added with getCustomer()

---
+++ Web Project/src/com/project/spring/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.project.spring.dao;
+
+import java.util.List;
+
+import com.project.spring.entity.Customer;
+
+public interface CustomerDAO {
+
+	
+	public List<Customer> getCustomer();
+}
