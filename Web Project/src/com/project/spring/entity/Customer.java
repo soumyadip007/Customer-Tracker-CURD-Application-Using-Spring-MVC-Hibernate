16b702ef5c2678cfeb80565962b72b9924359685
soumyadip007 <soumyadip.note@gmail.com>
2019-05-26 13:53:43 +0530

    Entity Class added

--- Web Project/src/com/project/spring/entity/Customer.java
+++ Web Project/src/com/project/spring/entity/Customer.java
@@ -34,5 +34,52 @@ public class Customer {
 	{
 		
 	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email + "]";
+	}
+	
 	
 }
